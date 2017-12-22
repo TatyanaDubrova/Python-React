3ca5209fb4a9b65a75416dbbc7a707f9a340fe85
tdubrova <tatyana.dubrova2013@gmail.com>
2017-12-22 15:45:31 +0200

    get started with project

--- index.py
+++ index.py
@@ -3,7 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 from flask_bcrypt import Bcrypt
 
+print(BaseConfig.SQLALCHEMY_DATABASE_URI)
 app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
 app.config.from_object(BaseConfig)
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = BaseConfig.SQLALCHEMY_DATABASE_URI
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
